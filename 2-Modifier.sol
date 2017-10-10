37146dce042e2bc4480e6c859eb8650ee0cf6e0a
kkozlovsky <kkozlovsky@yandex.ru>
2017-10-10 23:49:38 +0300

    Added Modifier example

---
+++ 2-Modifier.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.4.0;
+contract HelloWorldContract {
+    string word = "Привет, пока!";
+    address issuer;
+    
+    function HelloWorldContract() public {
+        issuer = msg.sender;
+    }
+    
+    modifier ifIssuer() {
+        if (issuer != msg.sender) {
+            throw;
+        } else {
+            _; // continue 
+        }
+    }
+    
+    function getWord() public constant returns (string) {
+        return word;
+    }
+    
+    function setWord(string newWord) ifIssuer public returns(string) {
+        if (issuer != msg.sender) {
+            return "Вы не создатель!";
+        }
+        else {
+            word = newWord;
+            return "Вы создатель!";
+        }
+    }
+}
